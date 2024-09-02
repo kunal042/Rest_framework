c634d14cc517bf65457d3ac7385f191347f5a019
kunal042 <kunalroy199915@gmail.com>
2024-09-03 00:26:02 +0530

    viewset

--- restframe/api/views.py
+++ restframe/api/views.py
@@ -21,6 +21,8 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
 
+
+
 @api_view(['GET', 'POST', 'PUT', 'PATCH', "DELETE"])
 def student_list(request, pk = None):
     pk = pk 
@@ -178,5 +180,144 @@ class RUDStudent(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyMo
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)    
 
+
+#  Genric Api Views
+
+
+from rest_framework.generics import (ListAPIView, 
+CreateAPIView,RetrieveAPIView,UpdateAPIView,
+ListCreateAPIView,RetrieveUpdateAPIView,RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView, DestroyAPIView)
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+class StudentCreate(CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+class StudentRetrive(RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+class StudentUpdate(UpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+class StudentListCreate(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+class StudentRetriveUpdate(RetrieveUpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+class StudentRetriveDestroy(RetrieveDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+
+class StudentRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+
+
+
+# Views Set 
+from rest_framework import viewsets
+class StudentViewset(viewsets.ViewSet):
+    def list(self, request):
+        print("**************List****************")
+        print("Basename:", self.basename)
+        print("Action:", self.action)
+        print("Details:", self.detail)
+        print("Suffix:", self.suffix)
+        print("Name:", self.name)
+        print("Distription : ", self.description)
+
+        stu = Student.objects.all()
+        serializer = StudentSerializers(stu, many=True)
+        return Response(serializer.data)
+    
+    def retrive(self, request, pk=None):
+        print("**************Retrive****************")
+        print("Basename:", self.basename)
+        print("Action:", self.action)
+        print("Details:", self.detail)
+        print("Suffix:", self.suffix)
+        print("Name:", self.name)
+        print("Distription : ", self.description)
+
+        pk = pk
+        if pk is not None:
+            stu = Student.objects.get(pk=pk)
+            serializer = StudentSerializers(stu)
+            return Response(serializer.data)
+        
+    def create(self, request):
+        print("**************Create****************")
+        print("Basename:", self.basename)
+        print("Action:", self.action)
+        print("Details:", self.detail)
+        print("Suffix:", self.suffix)
+        print("Name:", self.name)
+        print("Distription : ", self.description)
+
+        serializer = StudentSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg" : " Data Created!!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+    
+    def update(self, request, pk):
+        print("**************update****************")
+        print("Basename:", self.basename)
+        print("Action:", self.action)
+        print("Details:", self.detail)
+        print("Suffix:", self.suffix)
+        print("Name:", self.name)
+        print("Distription : ", self.description)
+
+        pk = pk
+        stu = Student.objects.get(pk=pk)
+        serializers = StudentSerializers(stu, data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response({"MSg" : "Data is Complete Update!!"})
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk):
+        print("**************partial_update****************")
+        print("Basename:", self.basename)
+        print("Action:", self.action)
+        print("Details:", self.detail)
+        print("Suffix:", self.suffix)
+        print("Name:", self.name)
+        print("Distription : ", self.description)
+
+        pk = pk
+        stu = Student.objects.get(pk=pk)
+        serializers = StudentSerializers(stu, data=request.data, partial=True)
+        if serializers.is_valid():
+            serializers.save()
+            return Response({"Msg" : "Data is Partical Update!!!"})
+        return Response(serializers.errors)
+
+    def destory(self, request, pk):
+        print("**************partial_update****************")
+        print("Basename:", self.basename)
+        print("Action:", self.action)
+        print("Details:", self.detail)
+        print("Suffix:", self.suffix)
+        print("Name:", self.name)
+        print("Distription : ", self.description)
+
+        stu = Student.objects.get(pk=pk)
+        stu.delete()
+        return Response({"Msg" : "Data is Delete!!!"})                    
     
 
+
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
--- restframe/restframe/urls.py
+++ restframe/restframe/urls.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 
+from rest_framework.routers import DefaultRouter
+
+route = DefaultRouter()
+route1 = DefaultRouter()
+route.register('studentapi', views.StudentViewset, basename='studentapi')
+route1.register('studentapi1', views.StudentModelViewset, basename='studentapi1')
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -30,6 +37,23 @@ urlpatterns = [
     path('stulc/<int:pk>/', views.RUDStudent.as_view()),
 
     
+    # Generic Api
+
+    path("stulist/", views.StudentList.as_view()),
+    path("stucreate/", views.StudentCreate.as_view()),
+    path("stureterive/<int:pk>/", views.StudentRetrive.as_view()),
+    path("stuupdate/<int:pk>/", views.StudentUpdate.as_view()),
+
+    path("slc/", views.StudentListCreate.as_view()),
+    path("slc/<int:pk>/", views.StudentRetriveUpdate.as_view()),
+    path("slcd/<int:pk>/", views.StudentRetriveDestroy.as_view()),
+    path("slrcd/<int:pk>/", views.StudentRetriveUpdateDestroy.as_view()),
+
+
+    # ViewSet
+    path("", include(route.urls)),
+    path("api/", include(route1.urls)),
+
 
 ]
 
